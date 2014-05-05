529771ac14958964513655c016f562ef01eaeca0
ckhajavi <cara.khajavi@gmail.com>
2014-05-04 23:26:55 -0700

    added stockList class

--- stock.h
+++ stock.h
@@ -3,22 +3,29 @@
 
 
 #include<QString>
+#include "loginpage.h"
+
 //Think about this user Finance friend class of user
 class Stock
 {
 public:
     Stock();
     Stock(QString);
-    bool buy( int);
-    bool sell(int);
+    double buy(int, double);
+    double sell(int);
     bool isOwned();
-
+    void addToFavorites();
 
 private:
     int numOfShares;
     QString ticker;
-
-
+    bool owned;
+    double currentPrice;
+    double changeInPrice;
+    double openPrice;
+    double todayHigh;
+    double todayLow;
+    double volume;
 };
 
 #endif // STOCK_H
---
+++ stocklist.h
@@ -0,0 +1,13 @@
+#ifndef STOCKLIST_H
+#define STOCKLIST_H
+
+class StockList
+{
+public:
+    StockList();
+
+private:
+
+};
+
+#endif // STOCKLIST_H
