3db4f433e37fce1a44636465a63527c39ed7e054
ckhajavi <cara.khajavi@gmail.com>
2014-05-07 11:48:08 -0700

    added stockList Functionality

--- stocklist.cpp
+++ stocklist.cpp
@@ -4,7 +4,10 @@ StockList::StockList()
 {
     stockMap = new QMap<QString, Stock>;
     totalSpent = 0;
-    stockTotalValue= 0;
+    stockTotal = 0;
+    totalSpent = 0; //total spent on stocks with bought for price;
+    todaysGains = 0;
+    totalGains = 0;
 }
 
 bool StockList::saveStockList()
@@ -19,7 +22,7 @@ bool StockList::loadStockList()
 
 void StockList::addStock(const Stock& newStock)
 {
-    if (stockMap->contains(newStock.getTicker()) && newStock.isOwned())
+    if (stockMap->contains(newStock.getTicker()) && newStock.isOwned()) //if stock is already owned
     {
         Stock temp;
         temp = newStock + stockMap->value(newStock.getTicker());
@@ -29,14 +32,19 @@ void StockList::addStock(const Stock& newStock)
     {
         stockMap->insert(newStock.getTicker(), newStock);
     }
+    //update member variables stockTotalValue, and totalSpent
+    setTotalSpent();
+    setStockTotal();
+    setTodaysGains();
+    setTotalGains();
     QMap<QString, Stock>::const_iterator i = stockMap->constBegin(); //using an iterator to iterate through the Map
     while (i != stockMap->constEnd()) {
         qDebug() << i.key() << " : " << i.value().getCost() << endl;
         ++i;
     }
 }
-
-double StockList::totalCost()
+//sets totalSpent
+void StockList::setTotalSpent()
 {
     double totalCost = 0;  //temp value to hold total spent
     QMap<QString, Stock>::const_iterator i = stockMap->constBegin(); //using an iterator to iterate through the Map
@@ -45,22 +53,21 @@ double StockList::totalCost()
         ++i;
     }
     totalSpent = totalCost;  //change member variable
-    return totalCost;
 }
 
-double StockList::stockTotal()
+//sets stockTotalValue
+void StockList::setStockTotal()
 {
-    double stockTotal = 0;
+    double temp = 0;
     QMap<QString, Stock>::const_iterator i = stockMap->constBegin(); //using an iterator to iterate through the Map
     while (i != stockMap->constEnd()) {
-        stockTotal = stockTotal + i.value().getLatestPrice();
+        temp = temp + i.value().getLatestPrice();
         ++i;
     }
-    stockTotalValue = stockTotal;
-    return stockTotal;
+    stockTotal = temp;
 }
 
-double StockList::totalGain()
+void StockList::setTotalGains()
 {
     /*double totalGain = 0;
     QMap<QString, Stock>::const_iterator i = stockMap->constBegin(); //using an iterator to iterate through the Map
@@ -73,11 +80,14 @@ double StockList::totalGain()
     double totalCost = totalCost();
     gains = stockTotal - totalCost;
     return gains;*/
-    return (stockTotal() - totalCost());
+    setStockTotal();
+    setTotalSpent();
+    totalGains = getStockTotal() - getTotalSpent();
+
 
 }
 
-double StockList::todaysGains()
+void StockList::setTodaysGains()
 {
     double gains;
     QMap<QString, Stock>::const_iterator i = stockMap->constBegin(); //using an iterator to iterate through the Map
@@ -85,7 +95,27 @@ double StockList::todaysGains()
         gains = gains + i.value().getLatestPrice() - i.value().getOpenPrice();
         ++i;
     }
-    return gains;
+    todaysGains = gains;
+}
+
+double StockList::getStockTotal()
+{
+    return stockTotal;
+}
+
+double StockList::getTotalSpent()
+{
+    return totalSpent;
+}
+
+double StockList::getTodaysGains()
+{
+    return todaysGains;
+}
+
+double StockList::getTotalGains()
+{
+    return totalGains;
 }
 
 StockList::~StockList(){
