39066aa13bf1e9a6e21ccb443b1846166ec88d75
ckhajavi <cara.khajavi@gmail.com>
2014-05-16 13:48:42 -0700

    new user error checking
    the necessary fields are email, and password

--- newusersetup.cpp
+++ newusersetup.cpp
@@ -11,8 +11,39 @@ NewUserSetup::NewUserSetup(QWidget *parent) :
 bool NewUserSetup::validate()
 {
     //check to see if info matches input wanted still need to implement
-    checked = true;
-    return true;
+    QPalette palette = ui->lineEdit_password->palette();
+    palette.setColor(QPalette::Base, QColor(255,0,0));
+    QPalette palette2 = ui->lineEdit_email->palette();
+    palette2.setColor(QPalette::Base, QColor(255,0,0)); //Red
+    int count = 0; //counts the number of errors
+    if (ui->lineEdit_password->text() != ui->lineEdit_checkPassword->text())
+    {
+        ui->lineEdit_password->setPalette(palette);
+        ui->lineEdit_checkPassword->setPalette(palette);
+        ++count;
+    }
+    if(ui->lineEdit_email->text() != ui->lineEdit_checkEmail->text())
+    {
+        ui->lineEdit_email->setPalette(palette2);
+        ui->lineEdit_checkEmail->setPalette(palette2);
+        ++count;
+    }
+    if (ui->lineEdit_password->text() == "")
+    {
+        ui->lineEdit_password->setPalette(palette);
+        ++count;
+    }
+    if(ui->lineEdit_email->text() == "")
+    {
+        ui->lineEdit_email->setPalette(palette2);
+        ++count;
+    }
+    if( count > 0)
+    {
+        return false;
+    }
+    else
+        return true;
 }
 
 NewUserSetup::~NewUserSetup()
@@ -23,9 +54,19 @@ NewUserSetup::~NewUserSetup()
 void NewUserSetup::on_pushButton1_clicked()
 {
     //combo boxes are commented out, need to figure out how to grab text from combo box, also combo boxes produce an error on Mac OSX
+    //reset the lineEdits to the color white
+    QPalette palette = ui->lineEdit_password->palette();
+    palette.setColor(QPalette::Base, QColor(255,255,255));
+    ui->lineEdit_password->setPalette(palette);
+    ui->lineEdit_checkPassword->setPalette(palette);
+    QPalette palette2 = ui->lineEdit_email->palette();
+    palette2.setColor(QPalette::Base, QColor(255,255,255)); //Red
+    ui->lineEdit_email->setPalette(palette2);
+    ui->lineEdit_checkEmail->setPalette(palette2);
+
     User currentUser;
-
-
+    if (validate())
+    {
         currentUser.setPassword(ui->lineEdit_password->text());
         currentUser.setFName( ui->lineEdit_fName->text());
         currentUser.setMName(ui-> lineEdit_mName ->text());
@@ -48,5 +89,10 @@ void NewUserSetup::on_pushButton1_clicked()
         currentUser.setUserFunds(10000);
         currentUser.setFileName();
         currentUser.saveUser();
+    }
+    else
+    {
+        qDebug() << "correct the fields in red" <<endl;
+    }
 
 }
