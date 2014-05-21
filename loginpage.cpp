b924f77e2b195f87917d7760f653616ae6f6df0d
Priig <mprigmore1@gmail.com>
2014-05-20 23:27:09 -0700

    hjhj
    hjhj

--- loginpage.cpp
+++ loginpage.cpp
@@ -172,7 +172,6 @@ void LoginPage::on_btnBuyShares_clicked()
     int stockPrice = ui->lineEditCalculatePurchase->text().toDouble();
     qDebug() << stockPrice;
 
-    //Never enters first if statement not matter what, they return 0 for both for some reason...
     if (ui->lineEdit_TotalCash->text().toDouble() < ui->lineEditCalculatePurchase->text().toDouble()) //Not enough money to make trade.
     {
         QMessageBox::information(this, "Insufficient Funds", "Sorry, you do not have enough funds to make this purchase.");
